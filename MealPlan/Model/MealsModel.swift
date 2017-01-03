09095aabd911e1bdd26ebd0c78972f95e9d7ebc0
Chirag Gupta <gupta.chirag@gmail.com>
2017-01-03 23:38:21 +0100

    Add MealsModel that returns dummy meals

---
+++ MealPlan/Model/MealsModel.swift
@@ -0,0 +1,13 @@
+// MealPlan by Chirag Gupta
+
+struct MealsModel {
+    func getMeals() -> [String] {
+        return ["Butter chicken and rice",
+                "Quesadilla",
+                "Pizza",
+                "Rajma and rice",
+                "Momos",
+                "Broccoli pasta",
+                "Aloo paratha"]
+    }
+}
---
+++ MealPlanTests/Model/MealsModelTests.swift
@@ -0,0 +1,21 @@
+// MealPlan by Chirag Gupta
+
+import XCTest
+
+@testable import MealPlan
+
+class MealsModelTests : XCTestCase {
+
+    func testGetAllMeals() {
+        let mealsModel = MealsModel()
+        XCTAssertEqual(["Butter chicken and rice",
+                        "Quesadilla",
+                        "Pizza",
+                        "Rajma and rice",
+                        "Momos",
+                        "Broccoli pasta",
+                        "Aloo paratha"],
+                       mealsModel.getMeals())
+    }
+
+}
