18f22f092dfcd507646a422f721a5d86b8d8803c
Chirag Gupta <gupta.chirag@gmail.com>
2017-01-06 07:38:34 +0100

    Add a tableview with empty cells to MealPlan view

--- MealPlan/Views/MealPlanViewController.swift
+++ MealPlan/Views/MealPlanViewController.swift
@@ -7,14 +7,30 @@ protocol MealPlanView: class {
 }
 
 class MealPlanViewController: UIViewController, MealPlanView {
-    private var presenter: MealPlanPresenter!
+    @IBOutlet weak var tableView: UITableView!
+
+    var presenter: MealPlanViewPresenter!
+    fileprivate var meals = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        presenter = MealPlanPresenter(view: self)
+        presenter = presenter ?? MealPlanPresenter(view: self)
+        presenter.showMeals()
     }
 
     func set(meals: [String]) {
+        self.meals = meals
+    }
+}
+
+typealias TableViewImplementation = MealPlanViewController
+extension TableViewImplementation: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
--- MealPlanTests/Views/MealPlanViewControllerTests.swift
+++ MealPlanTests/Views/MealPlanViewControllerTests.swift
@@ -4,14 +4,34 @@ import XCTest
 
 @testable import MealPlan
 
+class MockMealPlanViewPresenter: MealPlanViewPresenter {
+    let view: MealPlanView
+
+    required init(view: MealPlanView, model: MealsModel = MealsModel()) {
+        self.view = view
+    }
+
+    func showMeals() {
+        view.set(meals: ["yummy", "food"])
+    }
+}
+
 class MealPlanViewControllerTests: XCTestCase {
     var viewController: MealPlanViewController!
+    var presenter: MealPlanViewPresenter!
 
     override func setUp() {
         super.setUp()
 
         viewController = createVC(identifier: "MealPlanViewController", storyboard: "Main") as! MealPlanViewController
+        presenter = MockMealPlanViewPresenter(view: viewController)
+        viewController.presenter = presenter
+
         viewController.assertView()
     }
 
+    func testCountOfMealsInList() {
+        let count = viewController.tableView(viewController.tableView, numberOfRowsInSection: 0)
+        XCTAssertEqual(count, ["yummy", "food"].count)
+    }
 }
