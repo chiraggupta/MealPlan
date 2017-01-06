fd18ea9bf6c28842dd4875eeeb30e4a7c3455976
Chirag Gupta <gupta.chirag@gmail.com>
2017-01-06 22:23:41 +0100

    Add Meal type

---
+++ MealPlan/Model/Meal.swift
@@ -0,0 +1,11 @@
+// MealPlan by Chirag Gupta
+
+import Foundation
+
+struct Meal: Equatable {
+    var title: String
+}
+
+func ==(lhs: Meal, rhs: Meal) -> Bool {
+    return lhs.title == rhs.title
+}
