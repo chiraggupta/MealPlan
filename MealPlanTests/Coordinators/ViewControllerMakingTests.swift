a6c026732be8ef1e714194ebb08479e653397a3f
Chirag Gupta <gupta.chirag@gmail.com>
2017-01-25 23:47:46 +0100

    Remove unnecessary describe for subject class

--- MealPlanTests/Coordinators/ViewControllerMakingTests.swift
+++ MealPlanTests/Coordinators/ViewControllerMakingTests.swift
@@ -7,56 +7,53 @@ import Nimble
 class ViewControllerMakingTests: QuickSpec {
     override func spec() {
         var subject: ViewControllerMakingClass!
-        describe("a ViewControllerMaking class") {
-            beforeEach {
-                subject = ViewControllerMakingClass()
+        beforeEach {
+            subject = ViewControllerMakingClass()
+        }
+        describe("making a view controller ") {
+            context("when it is embedded in a navigation controller") {
+                beforeEach {
+                    subject.viewControllerID = "Navigation_MealPlanViewController"
+                }
+
+                it("makes the right viewcontroller") {
+                    let viewController = subject.makeViewController()
+                    expect(viewController).to(beAKindOf(MealPlanViewController.self))
+                    expect(viewController.navigationController).to(beAKindOf(UINavigationController.self))
+                }
+
+                it("configures the viewcontroller") {
+                    expect(subject.makeViewController().title).to(equal("FooTitleConfigured"))
+                }
             }
 
-            describe("making a view controller ") {
-                context("when it is embedded in a navigation controller") {
-                    beforeEach {
-                        subject.viewControllerID = "Navigation_MealPlanViewController"
-                    }
-
-                    it("makes the right viewcontroller") {
-                        let viewController = subject.makeViewController()
-                        expect(viewController).to(beAKindOf(MealPlanViewController.self))
-                        expect(viewController.navigationController).to(beAKindOf(UINavigationController.self))
-                    }
-
-                    it("configures the viewcontroller") {
-                        expect(subject.makeViewController().title).to(equal("FooTitleConfigured"))
-                    }
+            context("when it is not embedded in a navigation controller") {
+                beforeEach {
+                    subject.viewControllerID = "MealPlanViewController"
                 }
 
-                context("when it is not embedded in a navigation controller") {
-                    beforeEach {
-                        subject.viewControllerID = "MealPlanViewController"
-                    }
+                it("makes the right viewcontroller") {
+                    expect(subject.makeViewController()).to(beAKindOf(MealPlanViewController.self))
+                }
+            }
 
-                    it("makes the right viewcontroller") {
-                        expect(subject.makeViewController()).to(beAKindOf(MealPlanViewController.self))
-                    }
+            context("when view controller doesn't exist in storyboard") {
+                beforeEach {
+                    subject.viewControllerID = "Foo"
                 }
 
-                context("when view controller doesn't exist in storyboard") {
-                    beforeEach {
-                        subject.viewControllerID = "Foo"
-                    }
+                it("raises an exception") {
+                    expect(subject.makeViewController()).to(raiseException())
+                }
+            }
 
-                    it("raises an exception") {
-                        expect(subject.makeViewController()).to(raiseException())
-                    }
+            context("when it doesn't exist") {
+                beforeEach {
+                    subject.viewControllerID = "MealsViewController"
                 }
 
-                context("when it doesn't exist") {
-                    beforeEach {
-                        subject.viewControllerID = "MealsViewController"
-                    }
-
-                    it("throws an error") {
-                        expect { () -> Void in _ = subject.makeViewController() }.to(throwAssertion())
-                    }
+                it("throws an error") {
+                    expect { () -> Void in _ = subject.makeViewController() }.to(throwAssertion())
                 }
             }
         }
@@ -67,7 +64,7 @@ class ViewControllerMakingTests: QuickSpec {
 
         var storyboardID = "Main"
         var viewControllerID = ""
-        
+
         func configure(viewController: MealPlanViewController) {
             viewController.title = "FooTitleConfigured"
         }
