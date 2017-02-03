e1d4c613bf3f19f7ad060b99e30e0b81ef944382
Chirag Gupta <gupta.chirag@gmail.com>
2017-02-03 23:33:53 +0100

    Add SelectMealFactory

---
+++ MealPlan/Views/Factories/SelectMealFactory.swift
@@ -0,0 +1,22 @@
+// MealPlan by Chirag Gupta
+
+import UIKit
+
+class SelectMealFactory: ViewControllerMaking {
+    private let day: DayOfWeek
+
+    init(day: DayOfWeek) {
+        self.day = day
+    }
+
+    typealias ViewControllerType = SelectMealViewController
+
+    var storyboardID = "Main"
+    var viewControllerID = "SelectMealViewController"
+
+    func makeViewController() -> UIViewController {
+        let vc = instantiate()
+        vc.presenter = SelectMealPresenter(day: day, view: vc)
+        return vc
+    }
+}
