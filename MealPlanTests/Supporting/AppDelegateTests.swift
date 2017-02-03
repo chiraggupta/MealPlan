f424dc2026c84c9ff47c9d24e11d658b820a33c5
Chirag Gupta <gupta.chirag@gmail.com>
2017-02-03 22:53:24 +0100

    Add tests for root view controller

---
+++ MealPlanTests/Supporting/AppDelegateTests.swift
@@ -0,0 +1,33 @@
+// MealPlan by Chirag Gupta
+
+import Quick
+import Nimble
+
+@testable import MealPlan
+
+class AppDelegateTests: QuickSpec {
+    override func spec() {
+        var root: UIViewController?
+        var displayed: UIViewController?
+
+        describe("root view controller on launch") {
+            beforeEach {
+                root = UIApplication.shared.keyWindow?.rootViewController
+                displayed = (root as? UINavigationController)?.topViewController
+            }
+
+            it("is a UINavigationController") {
+                expect(root).to(beAKindOf(UINavigationController.self))
+            }
+
+            it("embeds MealPlanViewController") {
+                expect(displayed).to(beAKindOf(MealPlanViewController.self))
+            }
+
+            it("has a presenter of type MealPlanPresenter") {
+                let presenter = (displayed as? MealPlanViewController)?.presenter
+                expect(presenter).to(beAKindOf(MealPlanPresenter.self))
+            }
+        }
+    }
+}
