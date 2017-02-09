795b9b0e83f851e090760adb20e8cadaf6f76c6a
Chirag Gupta <gupta.chirag@gmail.com>
2017-02-09 22:51:05 +0100

    Introduce CoreData (UserDefaults doesn't scale at all)

---
+++ MealPlan/Model/CoreData/NSPersistentContainer+Make.swift
@@ -0,0 +1,21 @@
+// MealPlan by Chirag Gupta
+
+import CoreData
+
+extension NSPersistentContainer {
+    static func make(descriptions: [NSPersistentStoreDescription] = []) -> NSPersistentContainer {
+        let container = NSPersistentContainer(name: "MealPlan")
+        container.persistentStoreDescriptions = descriptions
+
+        if descriptions.count > 0 {
+            container.persistentStoreDescriptions = descriptions
+        }
+
+        container.loadPersistentStores { (_, error) in
+            if let error = error as NSError? {
+                assertionFailure("ERROR: \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }
+}
---
+++ MealPlanTests/Model/CoreData/NSPersistentContainer_MakeTests.swift
@@ -0,0 +1,37 @@
+// MealPlan by Chirag Gupta
+
+import Quick
+import Nimble
+import CoreData
+@testable import MealPlan
+
+class NSPersistentContainerTests: QuickSpec {
+    override func spec() {
+        var subject: NSPersistentContainer!
+        describe("creating a persistent container") {
+            context("with default descriptions") {
+                beforeEach {
+                    subject = NSPersistentContainer.make()
+                }
+
+                it("is names MealPlan") {
+                    expect(subject.name).to(equal("MealPlan"))
+                }
+
+                it("has a view context of main concurrency type") {
+                    expect(subject.viewContext.concurrencyType)
+                        .to(equal(NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType))
+                }
+            }
+
+            context("at a non existing location") {
+                it("throws a fatal error") {
+                    let description = NSPersistentStoreDescription(url: URL(fileURLWithPath: "foo/bar"))
+
+                    expect { () -> Void in _ = NSPersistentContainer.make(descriptions: [description])}
+                        .to(throwAssertion())
+                }
+            }
+        }
+    }
+}
