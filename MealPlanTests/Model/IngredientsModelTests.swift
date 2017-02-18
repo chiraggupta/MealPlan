b1951da2712e72a0f5dd29c6e82e93578cfc821a
Chirag Gupta <gupta.chirag@gmail.com>
2017-02-18 22:39:37 +0100

    Add test for saving context while adding ingredients

--- MealPlanTests/Model/IngredientsModelTests.swift
+++ MealPlanTests/Model/IngredientsModelTests.swift
@@ -7,20 +7,35 @@ import Nimble
 class IngredientsModelTests: QuickSpec {
     override func spec() {
         var subject: IngredientsModel!
+        var contextProvider: ContextProviding!
 
         beforeEach {
-            subject = IngredientsModel(contextProvider: makeInMemoryPersistenContainer())
+            contextProvider = makeInMemoryPersistenContainer()
+            subject = IngredientsModel(contextProvider: contextProvider)
         }
 
         describe("adding ingredients") {
             beforeEach {
                 subject.add(ingredients: ["peanut cheese", "oil balls"])
             }
-            it("stores the ingredients") {
+            it("has the added ingredients") {
                 let ingredients = subject.getIngredients()
                 expect(ingredients.count) == 2
                 expect(ingredients).to(contain(["peanut cheese", "oil balls"]))
             }
+
+            describe("saving behaviour") {
+                beforeEach {
+                    contextProvider.mainContext.reset()
+                }
+
+                it("the added ingredients are persisted across resets") {
+                    let ingredients = subject.getIngredients()
+                    expect(ingredients.count) == 2
+                    expect(ingredients).to(contain(["peanut cheese", "oil balls"]))
+                }
+            }
+
             describe("adding duplicates") {
                 beforeEach {
                     subject.add(ingredients: ["peanut cheese", "bitter balls"])
